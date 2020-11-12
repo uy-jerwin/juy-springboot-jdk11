5fec49253eefc988614e08e68b7cdaddfeae3e1a
Uy, Jerwin <uy.jerwin@gmail.com>
2020-11-13 04:43:23 +0800

    added sample switch based on Spring Config

--- juy-springboot-jdk11-repo-mock/src/main/java/juy/repository/mock/config/AutoConfiguration.java
+++ juy-springboot-jdk11-repo-mock/src/main/java/juy/repository/mock/config/AutoConfiguration.java
@@ -1,8 +1,12 @@
 package juy.repository.mock.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty(name = "repo.mock", havingValue = "true", matchIfMissing = false)
 @Configuration
 @ComponentScan("juy.repository.mock")
 public class AutoConfiguration {
